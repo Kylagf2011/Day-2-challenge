2548aca73ecef1e4d7ae3d833b486039d9dc21b8
Anjali Sebastian <anjalisebastian@icloud.com>
2019-07-09 09:34:58 -0700

    First Commit

---
+++ Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+func findEven(num : Int){
+    if num % 2 == 0{
+        print("This number is even.")
+    }
+    else{
+        print("This number is odd.")
+    }
+}
+findEven(num: 7)
